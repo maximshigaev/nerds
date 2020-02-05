82ce5959f01bf4ca56f01cc0526c69ff0573675b
Maxim <financial_monster@mail.ru>
2020-02-05 18:27:02 +0400

    Markup

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+
+	output: {
+		filename: './js/bundle.js',
+		path: path.resolve(__dirname, './build')
+	},
+
+	devServer: {
+		overlay: true
+	},
+
+	// watch: true,
+
+	devtool: 'cheap-module-eval-source-map',
+
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true }
+					},
+					{
+						loader: 'postcss-loader',
+						options: { sourceMap: true, config: { path: 'postcss.config.js' } }
+					},
+					{
+						loader: 'sass-loader',
+						options: { sourceMap: true }
+					},
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: {
+					loader: "url-loader",
+					options: {
+						name: "[name].[ext]",
+						outputPath: 'fonts/'
+					}
+				}
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 1000,
+						name: '[name].[ext]',
+						outputPath: 'img/'
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.SourceMapDevToolPlugin({
+			filename: '[file].map'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'catalog.html',
+			template: 'src/catalog.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/style.css'
+		}),
+		new CopyWebpackPlugin([
+			{from: './src/img', to: './img'},
+			{from: './src/static', to: './static'},
+			{from: './src/fonts', to: './fonts'}
+		])
+	],
+	resolve: {
+		extensions: ['.js', '.json', '.jsx', '*']
+	}
+};
